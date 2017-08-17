672c8872d13002d5b67c7621e36b7e136f1d93fa
gabidavila <gabidavila@gmail.com>
2017-08-17 10:48:31 -0400

    added actions on rides and users

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,23 @@
 class Ride < ActiveRecord::Base
   belongs_to :user
   belongs_to :attraction
+
+  def take_ride
+    messages = ["Sorry."]
+    messages << "You do not have enough tickets to ride the #{self.attraction.name}." if self.user.tickets < self.attraction.tickets
+    messages << "You are not tall enough to ride the #{self.attraction.name}." if self.user.height < self.attraction.min_height
+
+    if messages.length == 1
+      return update_users_attributes
+    end
+
+    messages.join(" ")
+  end
+
+  def update_users_attributes
+    self.user.tickets -= self.attraction.tickets
+    self.user.nausea += self.attraction.nausea_rating
+    self.user.happiness += self.attraction.happiness_rating
+    self.user.save
+  end
 end
